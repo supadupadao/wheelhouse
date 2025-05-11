fbc84eaa6ad816b6b6ca06bc2859e96025c813ef
Suprun <hello@syoma.website>
2025-05-11 21:33:25 +0300

    feat: add API service (#10)

---
+++ api/app/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict, SettingsError
+
+from libs.error import IndexerSettingsError
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="api_")
+
+    log_level: str = "INFO"
+    db_url: str = "postgresql://postgres:password@localhost:5432/skipper"
+
+
+def init_settings() -> Settings:
+    try:
+        return Settings()
+    except SettingsError as err:
+        raise IndexerSettingsError(err)
