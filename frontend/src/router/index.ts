2b8ea222e99c54a864cf956ccc7d7bb3b19b089d
Suprun <hello@syoma.website>
2025-05-18 17:10:57 +0400

    chore: remove default components

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ProposalsList from '@/views/ProposalsList.vue'
+import ProposalPage from '@/views/ProposalPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'proposals_list',
+      component: ProposalsList,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/1',
+      name: 'proposal_page',
+      component: ProposalPage
     },
   ],
 })
---
+++ frontend/src/views/ProposalPage.vue
@@ -0,0 +1,3 @@
+<template>
+  Proposal Page
+</template>
